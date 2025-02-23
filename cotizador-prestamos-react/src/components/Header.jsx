d5f646bd2b75e83477491de4514849de72bb21ee
israelLuna13 <c24.e.israel.luna@gmail.com>
2025-02-23 13:30:48 -0600

    Primer commit

---
+++ cotizador-prestamos-react/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <>
+            <h1 className="text-4xl font-extrabold text-gray-500 text-center">
+                Cuanto <span className="text-blue-600">dinero</span> necesitas?
+            </h1>
+        </>
+    )
+}
